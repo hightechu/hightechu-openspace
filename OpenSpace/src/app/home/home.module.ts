bbfdb2b94235b998bc0836545f6b6af70e91c6c0
MatthewHightech <Smoose007@gmail.com>
2021-01-12 17:20:00 -0800

    gameScreen_Component_Addition

--- OpenSpace/src/app/home/home.module.ts
+++ OpenSpace/src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { GameScreenComponent } from './game-screen/game-screen.component'; 
 
 @NgModule({
   imports: [
@@ -14,6 +14,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, GameScreenComponent]
 })
 export class HomePageModule {}
