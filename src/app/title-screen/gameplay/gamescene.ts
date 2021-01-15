5e16d9111f867821bfc6b8f91008ad84b1ab545a
MatthewHightech <Smoose007@gmail.com>
2021-01-14 16:14:26 -0800

    Scrolling background and added gamedata service file

--- src/app/title-screen/gameplay/gamescene.ts
+++ src/app/title-screen/gameplay/gamescene.ts
@@ -30,32 +30,42 @@ export class GameScene extends Phaser.Scene {
     create(): void {
         this.gameplay = (this.scene.scene.game as any).gameInstanceService;
 
+        const width = this.scale.width;
+        const height = this.scale.height; 
         // backgrounds
-        this.starmap = this.add.image(0, 0, 'stars').setOrigin(0, 0).setScale(0.6);
+        this.starmap = this.add.tileSprite(0, 0, width, height, 'stars').setOrigin(0, 0); 
 
         // player
-        this.ship = this.physics.add.sprite(350, 490, 'ship').setScale(0.2);
+        this.ship = this.physics.add.sprite(350, 500, 'ship').setScale(0.2);
         this.ship.setCollideWorldBounds(true);
         this.ship.health = 100; 
 
         //healthBar
-        this.healthBar
+        this.healthBar = this.makeBar(20, 20, 0xFF0000);
+        this.healthBar.scaleX = 1; 
 
     }
 
     update(time): void {
         const cursors = this.input.keyboard.createCursorKeys();
 
+        // scrollbackground
+        this.starmap.tilePositionY -= 1; 
+        //this.starmap.tilePosition
         // player controls
         if (cursors.left.isDown) {
           this.ship.setVelocityX(-160);
-
+          
           //this.ship.anims.play('left', true);
         } else if (cursors.right.isDown) {
           this.ship.setVelocityX(160);
 
           //this.ship.anims.play('right', true);
+        } else {
+          this.ship.setVelocityX(0);
+          
         } // if/else if
+ 
 
     } // update function
 
@@ -68,7 +78,7 @@ export class GameScene extends Phaser.Scene {
       bar.fillStyle(color, 1);
 
       //fill the bar with a rectangle
-      bar.fillRect(0, 0, 200, 50);
+      bar.fillRect(0, 0, 200, 20);
       
       //position the bar
       bar.x = x;
@@ -78,8 +88,6 @@ export class GameScene extends Phaser.Scene {
       return bar;
   } // makeBar
 
-  setBarValue(bar,percentage) {
-    //scale the bar
-    bar.scaleX = percentage/100;
-  } // setbarvalue
+
+  
 } // gameScene class
